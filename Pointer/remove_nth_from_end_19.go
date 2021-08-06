b59d9d9f86fc2ee3f42e85f4391f1fae0f84d9ca
catbugdemo <2229587083@qq.com>
2021-08-06 19:38:36 +0800

    1

---
+++ Pointer/remove_nth_from_end_19.go
@@ -0,0 +1,23 @@
+package Pointer
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	node := &ListNode{0,head}
+	slow,fast := node,node
+	for i := 0; i <=n; i++ {
+		fast = fast.Next
+	}
+
+	for fast!=nil {
+		fast= fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return node.Next
+}
